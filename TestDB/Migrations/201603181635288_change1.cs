198ad30650d47a6e6144e890d405772dc9a54cb0
Mitchell Dowdy <mitchelldowdy1@gmail.com>
2016-03-18 11:38:33 -0500

    Test

---
+++ TestDB/Migrations/201603181635288_change1.cs
@@ -0,0 +1,26 @@
+namespace TestDB.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class change1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Class1",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Class1");
+        }
+    }
+}
---
+++ TestDB/Models/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestDB.Models
+{
+    public class Class1
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+}
